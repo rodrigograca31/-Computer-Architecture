5f1c1500660fc5ed927ea31cda3dd7d72e44e314
Rodrigo Graça <rodrigograca31@gmail.com>
2020-05-21 13:19:18 +0100

    docs: 📝 function documentation and some light refactoring

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -3,19 +3,6 @@
 import sys
 import re
 
-LDI = 0b10000010  # Load "Immediate"
-ADD = 0b10100000  # ALU function
-SUB = 0b10100001  # ALU function
-MUL = 0b10100010  # ALU function
-DIV = 0b10100011  # ALU function
-MOD = 0b10100100  # ALU function
-PRN = 0b01000111  # Print
-HLT = 0b00000001  # Halt
-PUSH = 0b01000101  # Push
-POP = 0b01000110  # Pop
-CALL = 0b01010000  # Call
-RET = 0b00010001  # Return to
-
 
 class CPU:
     """Main CPU class."""
@@ -23,26 +10,26 @@ class CPU:
     def __init__(self):
         """Construct a new CPU."""
         self.running = False    # Self explanatory
-        self.pc = 0             # Program Counter, address of the currently executing instruction
+        self.PC = 0             # Program Counter, address of the currently executing instruction
         self.reg = [0] * 8      # Registers, R0-R7, to hold values
-        # Register 7 is the Stack Pointer (index of register to know where the stack is at) self.reg[sp] += 1
+        # Register 7 is the Stack Pointer (index of register that knows where the stack is at) self.reg[sp] += 1
         self.sp = 7
-        # self.ram[self.reg[self.sp]] - 244 - Its the top of the stack and grows down
+        # self.ram[self.reg[self.sp]] = 244 - Is the top of the stack and grows down
         self.reg[self.sp] = 0xF4
         self.ram = [0] * 256    # RAM to load the program into.
         self.branchtable = {    # branchtable avoids if/elif statements by using an index to know which function to run
-            LDI: self.LDI,
-            ADD: self.ADD,
-            SUB: self.SUB,
-            MUL: self.MUL,
-            DIV: self.DIV,
-            MOD: self.MOD,
-            PRN: self.PRN,
-            HLT: self.HLT,
-            PUSH: self.PUSH,
-            POP: self.POP,
-            CALL: self.CALL,
-            RET: self.RET
+            0b10000010: self.LDI,   # Load "Immediate"
+            0b10100000: self.ADD,   # ALU function
+            0b10100001: self.SUB,   # ALU function
+            0b10100010: self.MUL,   # ALU function
+            0b10100011: self.DIV,   # ALU function
+            0b10100100: self.MOD,   # ALU function
+            0b01000111: self.PRN,   # Print
+            0b00000001: self.HLT,   # Halt
+            0b01000101: self.PUSH,  # Push
+            0b01000110: self.POP,   # Pop
+            0b01010000: self.CALL,  # Call
+            0b00010001: self.RET    # Return to
         }
 
     def load(self, args):
@@ -68,32 +55,9 @@ class CPU:
             print("usage: ./ls8.py <filename>")
             sys.exit(1)
 
-        # print(self.ram)
-
-        # For now, we've just hardcoded a program:
-
-        # program = [
-        #     # From print8.ls8
-        #     0b10000010,  # LDI R0,8
-        #     0b00000000,
-        #     0b00001000,
-        #     0b01000111,  # PRN R0
-        #     0b00000000,
-        #     0b00000001,  # HLT
-        # ]
-
-        # for instruction in program:
-        #     self.ram[address] = instruction
-        #     address += 1
-
-    '''
-    Arithmetic Logic Instructions
-    '''
-
     def alu(self, op, reg_a, reg_b):
-        """ALU operations."""
+        """ALU (Arithmetic Logic Instructions) operations."""
 
-        # print("Operation: ", op)
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
         elif op == "SUB":
@@ -114,12 +78,12 @@ class CPU:
         """
 
         print(f"TRACE: %02X | %02X %02X %02X |" % (
-            self.pc,
+            self.PC,
             # self.fl,
             # self.ie,
-            self.ram_read(self.pc),
-            self.ram_read(self.pc + 1),
-            self.ram_read(self.pc + 2)
+            self.ram_read(self.PC),
+            self.ram_read(self.PC + 1),
+            self.ram_read(self.PC + 2)
         ), end='')
 
         for i in range(8):
@@ -127,16 +91,14 @@ class CPU:
 
         print()
 
-    def ram_read(self, pc=None):
-        if pc == None:
-            pc = self.pc
-        # self.pc += 1
-        return self.ram[pc]
+    def ram_read(self, position):
+        return self.ram[position]
 
-    def ram_write(self, pc, value):
-        self.ram[pc] = value
+    def ram_write(self, position, value):
+        self.ram[position] = value
 
     def LDI(self, position, value):
+        """Load Immediate"""
         self.reg[position] = value
 
     def ADD(self, a, b):
@@ -164,44 +126,57 @@ class CPU:
     # pushes a given register to the stack
     def PUSH(self, register):
         self.reg[self.sp] -= 1
-        self.ram[self.reg[self.sp]] = self.reg[register]
+        self.ram_write(self.reg[self.sp], self.reg[register])
 
     # pops from the stack into a given register
     def POP(self, register):
-        self.reg[register] = self.ram[self.reg[self.sp]]
+        self.reg[register] = self.ram_read()[self.reg[self.sp]]
         self.reg[self.sp] += 1
 
     def CALL(self, register):
+        """
+        Saves the next instruction address in the stack to later return to
+        and sets the PC (Program Counter) to a given register value that stored where it wants to go/call
+        """
+
         self.reg[self.sp] -= 1
         # plus 2 because its the current instruction + the next one + the actual one it should come to later when it does RET
-        self.ram[self.reg[self.sp]] = self.pc + 2
+        self.ram_write(self.reg[self.sp], self.PC + 2)
         # minus two because this the operation size (op_size) is 1 and +1 after
-        self.pc = self.reg[register] - 2
+        self.PC = self.reg[register] - 2
 
     def RET(self):
+        """
+        Pops the next instruction address from the stack (the one that stored before it went somewhere)
+        and sets the PC (Program Counter) to it so that it returns to a given instruction
+        """
+
         # minus 1 because this the operation size (op_size) does +1 after
-        self.pc = self.ram[self.reg[self.sp]] - 1
+        self.PC = self.ram_read(self.reg[self.sp]) - 1
         self.reg[self.sp] += 1
 
     def run(self):
         """Run the CPU."""
+
         self.running = True
         op_size = 0  # operation size
 
         while self.running:
-            # print("read:", self.pc)
-            IR = self.ram_read(self.pc)  # Instruction Register
-            # print(f"read:{IR:b}")
+            IR = self.ram_read(self.PC)  # Instruction Register
 
-            # This does a bitwise operation to shift the current IR (Instruction Register) value by 6 bits in this >> direction
-            # so that 0b10000010 it turns into 0b00000010
-            # and 0b01000010 turns into 0b00000001
-            # that tells us how many "jumps" we have to increase by (0, 1 or 2)
-            # In other words: it turns the 2 "highest" bits into a new binary 00, 01, 10 that tells us how many operants will follow the current instruction
+            """
+            This does a bitwise operation to shift the current IR (Instruction Register) value by 6 bits in this >> direction
+            so that 0b10000010 turns into 0b00000010
+            and 0b01000010 turns into 0b00000001
+            (ads 6 zeros on the left side, pushing everything else right and "eliminating/clipping it")
+            that tells us how many "jumps" we have to increase by (0, 1 or 2)
+            In other words: it turns the 2 "highest" (left) bits into a new binary 00, 01, 10 
+            that tells us how many operants will follow the current instruction, 0, 1, 2
+            """
             op_size = (IR >> 6)
 
-            operand_a = self.ram_read(self.pc + 1)
-            operand_b = self.ram_read(self.pc + 2)
+            operand_a = self.ram_read(self.PC + 1)
+            operand_b = self.ram_read(self.PC + 2)
 
             # print(f"{IR:08b}")
             # print(f"{self.branchtable[IR]}")
@@ -214,7 +189,7 @@ class CPU:
                 elif op_size == 2:
                     self.branchtable[IR](operand_a, operand_b)
             except KeyError:
-                print(f"invalid instruction [{self.ram[self.pc]:08b}]")
+                print(f"invalid instruction [{self.ram[self.PC]:08b}]")
                 running = False
 
-            self.pc += (op_size+1)
+            self.PC += (op_size+1)
